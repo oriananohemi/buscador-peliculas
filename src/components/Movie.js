a017a49f7d5d34b4a395445b7e2cfe25d269ed5b
Oriana <oriana.nohemi22@gmail.com>
2020-09-14 11:51:13 -0500

    Crear un componente independiente que pinte la lista de peliculas por separado

---
+++ src/components/Movie.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class Movie extends Component {
+  static propTypes = {
+    title: PropTypes.string,
+    year: PropTypes.string,
+    poster: PropTypes.string
+  }
+
+  render() {
+    const { poster, title, year } = this.props
+
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img src={poster} alt={title}/>
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            <p className="title is-4">{title}</p>
+            <p className="subtitle is-6">{year}</p>
+          </div>
+        </div>
+
+        <div className="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+          <a href="#">#css</a> <a href="#">#responsive</a>
+          <br/>
+          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        </div>
+      </div>
+    </div>
+    )
+  }
+}
