439fdbbaebc7e80b59703fc84de11ef3d39d5a31
Oriana <oriana.nohemi22@gmail.com>
2020-09-14 17:52:21 -0500

    Updates

---
+++ precache-manifest.759af452deec528007d6e5b4245550c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a64043b5436d02c4f7369ffc074349cc",
+    "url": "/oriananohemi/buscador-peliculas/index.html"
+  },
+  {
+    "revision": "bca96e0f480a92348288",
+    "url": "/oriananohemi/buscador-peliculas/static/css/2.3807f13a.chunk.css"
+  },
+  {
+    "revision": "49e42c17813cc2b02d8e",
+    "url": "/oriananohemi/buscador-peliculas/static/css/main.f719e279.chunk.css"
+  },
+  {
+    "revision": "bca96e0f480a92348288",
+    "url": "/oriananohemi/buscador-peliculas/static/js/2.38cb9bd5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/oriananohemi/buscador-peliculas/static/js/2.38cb9bd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49e42c17813cc2b02d8e",
+    "url": "/oriananohemi/buscador-peliculas/static/js/main.eccc04e8.chunk.js"
+  },
+  {
+    "revision": "a2ea17b42766c3d2a271",
+    "url": "/oriananohemi/buscador-peliculas/static/js/runtime-main.1477e3c8.js"
+  },
+  {
+    "revision": "ed2cb85b7b64639d3a25d26d1d68ffe2",
+    "url": "/oriananohemi/buscador-peliculas/static/media/fondo.ed2cb85b.jpg"
+  }
+]);
