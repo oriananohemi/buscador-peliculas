8efe1893fa28db114814e5215f5e2231e632be55
Oriana Nohemi Reina Marcano <49885209+oriananohemi@users.noreply.github.com>
2020-09-15 15:10:25 -0500

    Update ButtonBackToHome.js

--- src/components/ButtonBackToHome.js
+++ src/components/ButtonBackToHome.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const ButtonBackToHome = () => (
   <Link
     className="button is-info"
-    to="/">
+    to="/buscador-peliculas/">
     Volver a la portada
   </Link>
 )
